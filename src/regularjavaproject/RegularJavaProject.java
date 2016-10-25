f33484a67651e0815ce0f009f67b1aea85a32e8e
superuserrr <superuserrr@users.noreply.github.com>
2016-10-25 23:13:02 +0300

    Update RegularJavaProject.java
    method içine Output eklentisi yapıldı.

--- src/regularjavaproject/RegularJavaProject.java
+++ src/regularjavaproject/RegularJavaProject.java
@@ -15,7 +15,8 @@ public class RegularJavaProject {
      * @param args the command line arguments
      */
     public void methodlar(String value){
-        
+        System.out.println("Sayısal değerler");
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
